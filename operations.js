5516a15405a38395d5237dfe1932cbc033846b8a
Abit <abitmore@users.noreply.github.com>
2018-05-22 17:50:00 +0200

    Update operations.js

--- operations.js
+++ operations.js
@@ -1,5 +1,5 @@
-export const call_order_update_operation_options = new Serializer(
-    "call_order_update_operation_options", {
+export const call_order_update_options = new Serializer(
+    "call_order_update_options", {
     target_collateral_ratio: optional(uint16)
 }
 );
@@ -10,6 +10,6 @@ export const call_order_update = new Serializer(
     funding_account: protocol_id_type("account"),
     delta_collateral: asset,
     delta_debt: asset,
-    extensions: extension(call_order_update_operation_options)
+    extensions: extension(call_order_update_options)
 }
 );
